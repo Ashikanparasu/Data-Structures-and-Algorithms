63e5c162a1d7ea9a1de08d71f831025850a4f10e
Ashikanparasu <ashikanparasu@gmail.com>
2025-03-27 07:36:39 +0530

    Node1

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,21 @@
+
+public class LinkedList{
+
+    Node head ;
+
+    class Node{
+        int datd;
+        Node next;
+    }
+
+
+    LinkedList(){
+        head = null;
+    }
+
+ public void insertAtBeginning(int val){
+    Node newNode = new Node(val);
+
+    }
+    
+}

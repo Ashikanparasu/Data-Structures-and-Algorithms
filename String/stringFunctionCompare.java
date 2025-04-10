2fea425acaac97dc93fba2bcf5e4f5c014d3957d
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-10 15:31:23 +0530

    stringFunctionCompare

---
+++ String/stringFunctionCompare.java
@@ -0,0 +1,15 @@
+public class stringFunctionCompare {
+
+    public static void main(String[] args) {
+        String Name1 = "Ashik A";
+        String Name2 = "Ashik A";
+
+        if (Name1.equals(Name2)){
+            System.out.println("Equal");
+        }
+        else{
+            System.err.println("Not Equal");
+        }
+    }
+    
+}

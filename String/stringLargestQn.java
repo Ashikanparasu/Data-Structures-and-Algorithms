f018cefe8328f8095550710f57b16f4c84b8b06b
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-10 19:21:24 +0530

    Largeststring

---
+++ String/stringLargestQn.java
@@ -0,0 +1,20 @@
+public class stringLargestQn {
+
+    public static void main(String[] args) {
+        
+        String Fruits [] = {"apple","mango","banana"};
+
+        String Largest = Fruits[0];
+        for(int i = 1;i <Fruits.length;i++){
+            if(Largest.compareTo(Fruits[i])<0){
+
+                Largest = Fruits[i];
+
+            }
+        }
+        System.out.println(Largest);
+
+    }
+
+    
+}

442faeb7fa09944e27e9ce8fb397c319b8766847
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-10 13:32:35 +0530

    Concatenate

---
+++ String/Concatenate.java
@@ -0,0 +1,15 @@
+public class Concatenate {
+    public static void main(String args[]){
+        String firstName;
+        String lastName;
+        String fullname;
+
+        firstName = "Ashik";
+        lastName = "Anparasu";
+        fullname = firstName + " " + lastName;
+
+        System.out.println(fullname);
+
+    }
+    
+}

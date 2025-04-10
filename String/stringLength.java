496814a0dcaaee4f240a45ae36391656303e1ae9
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-10 13:23:27 +0530

    stringLength

---
+++ String/stringLength.java
@@ -0,0 +1,8 @@
+public class stringLength {
+    public static void main (String args[]){
+        String Name = "Ashik A";
+        int Length = Name.length(); // Length Fuction
+        System.out.println(Length);
+    }
+    
+}

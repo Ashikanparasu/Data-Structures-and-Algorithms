db2161ec9fd5ffd0245a9a1af9892aae3fc8394e
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-12 19:53:59 +0530

    Multiple Using Functon Call

---
+++ Methods/ProductUsingMeth.java
@@ -0,0 +1,16 @@
+
+public class ProductUsingMeth {
+
+    public static int Multiply(int a ,int b){
+        int mul = a*b;
+        return mul;
+    }
+
+    public static void main(String args[]){
+
+
+        int Answer = Multiply(5,10);
+        System.out.println(Answer);
+    }
+    
+}

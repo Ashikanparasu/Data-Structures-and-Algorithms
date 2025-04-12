46417e75ea3d026fa3d5dc44b178a2ee0161cb92
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-12 19:21:12 +0530

    LinkedListSyntax

--- LinkedList/Collection/LinkedListCollectionSyntax.java
+++ LinkedList/Collection/LinkedListCollectionSyntax.java
@@ -1,5 +1,16 @@
 import java.util.LinkedList;
 
 public class LinkedListCollectionSyntax {
+
+    public static void main (String args[]){
+        LinkedList <Integer> ll = new LinkedList<> ();
+
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addFirst(2);
+        ll.addFirst(1);
+
+        System.out.println(ll);
+    }
     
 }

28190c87969f21171f558889f303b224ed69f86f
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-12 19:14:26 +0530

    LinkeList

---
+++ LinkedList/SyntaxLinkedList1.java
@@ -0,0 +1,20 @@
+
+
+public class SyntaxLinkedList1 {
+
+    public static class Node {
+        int Data;
+        Node Next;
+
+        public Node (int Data){
+            this.Data= Data;
+            this.Next = null;
+        }
+    }
+    
+
+    
+    public static void main(String args()){
+
+    }
+}

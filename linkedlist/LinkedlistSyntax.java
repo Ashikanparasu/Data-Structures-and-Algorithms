102ad1a352f8f7576ac7ac83f2098539724b46af
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-12 12:30:23 +0530

    Linkedlist

---
+++ linkedlist/LinkedlistSyntax.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+
+public class LinkedlistSyntax {
+
+    public static class Node{
+        int Data;
+        Node Next;
+
+        public Node (int Data){
+            this.Data = Data;
+            this.Next = null;
+        }
+    }
+    public static Node head;
+    public static Node Tail;
+
+    public static void main (String args[]){
+
+    }
+    
+}

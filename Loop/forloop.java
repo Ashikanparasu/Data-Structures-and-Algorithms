1a6ca4bea60395c98fcc816beb1ecdcfedb464dd
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-16 22:35:51 +0530

    up

---
+++ Loop/forloop.java
@@ -0,0 +1,13 @@
+
+
+public class forloop {
+
+    public static void main(String args[]){
+
+       int  n=10;
+    for(int i =0 ; n> i; i++){
+        System.out.println("Helloworld");
+    }
+    
+}
+}

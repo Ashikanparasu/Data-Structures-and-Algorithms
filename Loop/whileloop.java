6590037ab2eb574644f2a7bc301b34f9ce2c7582
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-16 22:06:32 +0530

    update

---
+++ Loop/whileloop.java
@@ -0,0 +1,16 @@
+
+
+public class whileloop {
+
+    public static void main (String args[]){
+
+        int count =0;
+
+        while(count < 100){
+            System.out.println("Helooworld");
+
+            count++;
+        }
+    }
+    
+}

7ff8d82560028ff62326425dc4a37ad1fa2d0def
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-16 23:01:17 +0530

    pyramid

---
+++ Pattern/InvertedStar.java
@@ -0,0 +1,13 @@
+
+public class InvertedStar {
+
+    public static void main(String args[]){
+        for(int i =1; i<=4;i++){
+            for(int star=1; star <= 4-i+1;star++){
+                System.out.print("*");
+
+            }System.out.println();
+        }
+    }
+    
+}

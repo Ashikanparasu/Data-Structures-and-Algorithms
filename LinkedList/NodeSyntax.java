83e93c2333862c2511aaaa3a4032df1d5c2708ac
Ashikanparasu <ashikanparasu@gmail.com>
2025-04-19 00:37:53 +0530

    Node Syntax

---
+++ LinkedList/NodeSyntax.java
@@ -0,0 +1,18 @@
+public class NodeSyntax {
+
+    public static class Node{
+
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next= null;
+        }
+    }
+
+    public static void main(String args[]){
+       
+    }
+    
+}

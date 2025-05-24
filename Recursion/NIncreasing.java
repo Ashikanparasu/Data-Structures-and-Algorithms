47fec9f9bed739fe9fe76bc2ee3ad61e6563a048
Ashikanparasu <ashikanparasu@gmail.com>
2025-05-24 22:12:42 +0530

    Recursion

---
+++ Recursion/NIncreasing.java
@@ -0,0 +1,17 @@
+
+
+public class NIncreasing {
+    public static void printn(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        printn(n-1);
+        System.out.print(n + " ");
+    }
+
+    public static void main (String args[]){
+        printn(10);
+    }
+    
+}

fede90d8e544b655ede0bda37a75971337522a81
Ashikanparasu <ashikanparasu@gmail.com>
2025-07-12 01:50:45 +0530

    StackLL

--- Stack/StackLL.java
+++ Stack/StackLL.java
@@ -2,35 +2,46 @@
 
 public class StackLL {
     
-    public static class Node {
-        int Data;
+    public static class Node{
+        int data;
         Node next;
 
-        public Node (int data){
-            this.Data = data;
+        public Node (int Data){
+            this.data = Data;
             this.next = null;
+        }
+    }
 
+   static Node head;
+   static Node tail;
+
+    public static void push (int val){
+        Node newNode = new Node(val);
+
+        if(head == null){
+            head = newNode;
         }
 
-        public static class CreateStack {
-            Linkedkist <Integer> s = new Linkedlist <> ();
+        newNode.next = head;
+        head = newNode;
+       
 
-            public static void Add(int data){
-                s.add(data);
-            }
+    }
 
-            public static int Remove(){'
-            
-            '   top = s.pop();
-                s.pop();
-                return top;
-            }
-
-            public static int Peek(){
-                return s.pop();
-            }
-
-
+    public static int pull(){
+        if(head == null){
+            return -1;
         }
+
+        int val = head.data;
+        head.next = head ;
+        return val;
+    }
+
+    public static void main(String [] args){
+
+        StackLL s = new StackLL();
+        s.push(1);
+
     }
 }

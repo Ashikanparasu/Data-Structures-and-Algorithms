ec7bf44bc83d1509c85f9218a2eaf0f678fed11a
Ashikanparasu <ashikanparasu@gmail.com>
2025-07-12 22:11:48 +0530

    StackAddAtBottom

---
+++ Stack/PushAtBottom.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+public class PushAtBottom {
+
+    public static void pushAtBottom(Stack<Integer> s , int val){
+        if(s.isEmpty()){
+            s.push(val);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s,val);
+        s.push(top);
+        return;
+    }
+
+    public static void main(String [] args){
+
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s,4);
+
+        while(! s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+
+
+    }
+    
+}

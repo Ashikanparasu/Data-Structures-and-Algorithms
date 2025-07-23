dec88b5280421b7ee3c5faf8a51ccb4995dd13ea
Ashikanparasu <ashikanparasu@gmail.com>
2025-07-23 10:43:52 +0530

    TreeMap

---
+++ HashMap/TreeMap1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class TreeMap1 {
+
+    public static void main(String [] args){
+
+
+        TreeMap<String , Integer> tm = new TreeMap<>();
+        tm.put("A",1);
+        tm.put("S",2);
+        tm.put("H",3);
+        tm.put("I",4);
+        tm.put("K",5);
+
+
+        System.out.println(tm);
+    }
+
+
+    
+}

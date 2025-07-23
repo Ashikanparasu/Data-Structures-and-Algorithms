fe0700a22306ddeafe26b434373312b0dab8a097
Ashikanparasu <ashikanparasu@gmail.com>
2025-07-23 10:39:32 +0530

    LinkedHashMap

---
+++ HashMap/LinkedHashMap1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LinkedHashMap1 {
+
+    public static void main(String [] args){
+
+
+        LinkedHashMap<String , Integer> lhm = new LinkedHashMap<>();
+        lhm.put("A",1);
+        lhm.put("S",2);
+        lhm.put("H",3);
+        lhm.put("I",4);
+        lhm.put("K",5);
+
+
+        System.out.println(lhm);
+    }
+
+
+    
+}
